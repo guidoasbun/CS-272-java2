5343c6d36fa50c20bcc5ea7326ed6d46d3c3999c
Guido Asbun <guido@asbun.io>
2022-09-06 13:19:09 -0700

    first commit

---
+++ src/Sep06/LabActivityObjectOrientedDesign/Test.java
@@ -0,0 +1,12 @@
+package LabActivityObjectOrientedDesign;
+
+
+public class Test {
+    public static void main(String[] args) {
+        GlobalPositioningLocation location1 = new GlobalPositioningLocation();
+        GlobalPositioningLocation location2 = new GlobalPositioningLocation(34.123, -118.456, 100, "meters");
+
+        System.out.println("Location 1: " + location1.getLatitude() + ", " + location1.getLongitude() + ", " + location1.getElevation() + " " + location1.getElevationUnits());
+        System.out.println("Location 2: " + location2.getLatitude() + ", " + location2.getLongitude() + ", " + location2.getElevation() + " " + location2.getElevationUnits());
+    }
+}
