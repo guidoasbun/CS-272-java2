bd400068fd4dcfc7776c12afceba08949a7a278a
guidoasbun <guido@asbun.io>
2022-09-17 00:05:49 -0700

    Project - Inheritance

--- src/JavaFX/ExtraCreditMainMenu/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module io.asbun.extracreditmainmenu {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens io.asbun.extracreditmainmenu to javafx.fxml;
-    exports io.asbun.extracreditmainmenu;
-}
---
+++ src/Projects/Inheritance/Part2/Quarter.java
@@ -0,0 +1,28 @@
+package Projects.Inheritance.Part2;
+
+/**
+ A quarter with a state theme.
+ */
+public class Quarter extends Coin
+{
+    // your work here
+    private final String state;
+    /**
+     Constructs a quarter.
+     @param aValue the monetary value of the quarter.
+     @param aName the name of the quarter
+     */
+    public Quarter(String aState)
+    {
+        // your work here
+        super(0.25, "Quarter");
+        state = aState;
+    }
+
+    @Override
+    public String getDescription()
+    {
+        // your work here
+        return super.getDescription() + ", state=" + state;
+    }
+}
