f011221eaca92b3a5e8cfb6b32de9660b5160999
guidoasbun <guido@asbun.io>
2022-09-30 19:30:48 -0700

    New folder structure

---
+++ src/Sep20ExceptionHandling/LabActivityExceptionHandling/Test.java
@@ -0,0 +1,10 @@
+package Sep20ExceptionHandling.LabActivityExceptionHandling;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Sphere sphere = new Sphere();
+        sphere.userInput();
+    }
+}
