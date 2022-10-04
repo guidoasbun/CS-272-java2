d4da617b91bd2dcfca2e45a9b7e56ed331ce7309
Guido Asbun <guido@asbun.io>
2022-10-04 09:57:48 -0700

    Lab Generics Cleanup

--- src/Sep29Generics/LabActivityGenerics/ArrayMethodTester.java
+++ src/Sep29Generics/LabActivityGenerics/ArrayMethodTester.java
@@ -6,7 +6,7 @@ public class ArrayMethodTester
 {
     public static void test1()
     {
-        int NUMBER_OF_BUILDINGS = 3;
+        final int NUMBER_OF_BUILDINGS = 3;
 
         Double[] buildingCosts = new Double[NUMBER_OF_BUILDINGS];
         ArrayMethod.initialize(1234567.89, buildingCosts);
@@ -19,14 +19,15 @@ public class ArrayMethodTester
 
         Double[] buildingCosts = new Double[NUMBER_OF_BUILDINGS];
 
-        Scanner buildingCost = new Scanner(System.in);
+        Scanner buildingCostInput = new Scanner(System.in);
         int count = 0;
         while (count < buildingCosts.length)
         {
             System.out.print("Enter the cost of building " + (count + 1) + ": ");
-            buildingCosts[count] = buildingCost.nextDouble();
+            buildingCosts[count] = buildingCostInput.nextDouble();
             count++;
         }
+        buildingCostInput.close();
         ArrayMethod.display("Building Costs (Euros)", buildingCosts);
     }
 }
