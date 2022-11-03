99b13a4e46e66338f476d634241cb84882209cc3
Guido Asbun <guido@asbun.io>
2022-11-03 13:07:58 -0700

    SinglyLinkedList concept check and lab

---
+++ src/Nov3LinkedLists/ConceptCheck/SinglyLinkedList.java
@@ -0,0 +1,18 @@
+package Nov3LinkedLists.ConceptCheck;
+
+public class SinglyLinkedList<E> {
+    // INSTANCE VARIABLES
+    private int size;
+    private SinglyLinkedListNode<E> head = null;
+    private SinglyLinkedListNode<E> tail = null;
+
+    // Add Java Code Segment Here
+
+    // CONSTRUCTORS
+    public SinglyLinkedList() {
+        size = 0;
+    }
+
+    // INSTANCE METHODS
+
+}
