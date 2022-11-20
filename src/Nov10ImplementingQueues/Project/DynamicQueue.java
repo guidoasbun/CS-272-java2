6f83cfc73f0bfc548082c27264b4678ce4871b5c
guidoasbun <guido@asbun.io>
2022-11-19 21:36:45 -0800

    implementing queue

---
+++ src/Nov10ImplementingQueues/Project/DynamicQueue.java
@@ -0,0 +1,50 @@
+package Nov10ImplementingQueues.Project;
+
+
+import Nov1ImplementingArrayLists.LabActivityImplementingArrayLists.DynamicArray;
+
+public class DynamicQueue<E> {
+    // INSTANCE VARIABLES
+    private DynamicArray<E> queue;
+
+    // CONSTRUCTORS
+    public DynamicQueue() // Create empty queue
+    {
+        queue = new DynamicArray<E>();
+    }
+
+    public DynamicQueue(E[] array) // Creates a queue that contain the elements from the array (first array element will be the first element in the queue, ... , last array element will be the last element in the queue)
+    {
+        queue = new DynamicArray<E>();
+        for (E element : array) {
+            queue.addToEnd(element);
+        }
+    }
+
+    // INSTANCE METHODS
+    public void offer(E element) {
+        queue.addToEnd(element);
+    }
+
+    public E poll() {
+        if (queue.getNumberOfElements() == 0) {
+            return null;
+        } else {
+            return queue.remove(0);
+        }
+    }
+
+    public E peek() {
+        if (queue.getNumberOfElements() == 0) {
+            return null;
+        } else {
+            return queue.get(0);
+        }
+    }
+
+    public int getSize() {
+        return queue.getNumberOfElements();
+    }
+
+
+}
